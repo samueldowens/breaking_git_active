887402fd3fbcd4b874f5553eab1abb62aa379a68
samueldowens <samueldowens@gmail.com>
2013-12-03 18:18:31 -0500

    lol

--- break.rb
+++ break.rb
@@ -1,1 +1,15 @@
-#
+500.times do
+
+a = rand(1..50).to_s
+b = rand(1..50).to_s
+c = rand(1..50).to_s
+d = rand(1..50).to_s
+e = rand(1..50).to_s
+
+name = a+b+c+d+e
+exec "touch #{name}.rb"
+exec "git add ."
+exec "git commit -m 'lol'"
+exec "git push origin master"
+exec "echo 'another'"
+end
